8013450f4ac77de7e996995a07cc55375ec26180
Karina135 <89375309+Karina135@users.noreply.github.com>
2022-04-16 21:47:08 +0500

    Create .h

---
+++ memory/Matrix/.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int** createMatrix(int rows, int cols);
+void print(int* matrix[], int rows, int cols);
+void destroyMatrix(int* matrix[]);
+void fill(int* matrix[], int rows, int cols);
